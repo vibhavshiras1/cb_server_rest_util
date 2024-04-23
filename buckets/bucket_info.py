0bc2ff066b0dba5fd0e5325aec082449a9ca3189
Ashwin <ashwintrojan+github@gmail.com>
2024-04-23 12:58:11 +0530

    Adding support to fetch basic_stats within 'get_bucket_info'

--- buckets/bucket_info.py
+++ buckets/bucket_info.py
@@ -6,7 +6,7 @@ class BucketInfo(CBRestConnection):
     def __init__(self):
         super(BucketInfo, self).__init__()
 
-    def get_bucket_info(self, bucket_name=None):
+    def get_bucket_info(self, bucket_name=None, basic_stats=False):
         """
         GET :: /pools/default/buckets
         docs.couchbase.com/server/current/rest-api/rest-buckets-summary.html
@@ -15,5 +15,7 @@ class BucketInfo(CBRestConnection):
         if bucket_name:
             bucket_name = quote(bucket_name)
             api += f"/{bucket_name}"
+        if basic_stats:
+            api += "?basic_stats=true"
         status, content, _ = self.request(api)
         return status, content
