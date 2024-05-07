373100edc9a9c6d4a283a910b1b347abeb3d2029
Vibhav S P <vibhavshiraspramod@gmail.com>
2024-05-07 11:07:41 +0530

    Adding update bucket API call

--- buckets/manage_bucket.py
+++ buckets/manage_bucket.py
@@ -41,6 +41,10 @@ class BucketManageAPI(CBRestConnection):
         POST :: /pools/default/buckets/<bucket_name>
         docs.couchbase.com/server/current/rest-api/rest-bucket-create.html
         """
+        api = self.base_url + f"/pools/default/buckets/{bucket_name}"
+        status, content, response = self.request(api, self.POST,
+                                           params=bucket_params)
+        return status, content, response
 
     def delete_bucket(self, bucket_name):
         """
