93a12a0063591f4a6faf11240917b3490b8988ac
Ashwin G <ashwin.govindarajulu@couchbase.com>
2024-06-18 10:56:10 +0530

    Adding support for cluster_nodes::statistics APIs

---
+++ cluster_nodes/statistics.py
@@ -0,0 +1,36 @@
+from cb_server_rest_util.connection import CBRestConnection
+
+
+class StatisticsAPI(CBRestConnection):
+    def __init__(self):
+        super(StatisticsAPI, self).__init__()
+
+    def get_prometheus_sd_config(self):
+        """
+        GET /prometheus_sd_config
+        docs.couchbase.com/server/current/rest-api/rest-discovery-api.html
+        """
+        api = self.base_url + "/prometheus_sd_config"
+        status, content, _ = self.request(api, CBRestConnection.GET)
+        return status, content
+
+    def get_stats_for_metric(self, metric_name, function_expression=None):
+        """
+        GET /pools/default/stats/range/<metric_name>/[function-expression]
+        docs.couchbase.com/server/current/rest-api/rest-statistics-single.html
+        """
+        api = self.base_url + f"/pools/default/stats/range/{metric_name}"
+        if function_expression:
+            api += f"?{function_expression}"
+        status, content, _ = self.request(api, CBRestConnection.GET)
+        return status, content
+
+    def get_multiple_stats(self, data):
+        """
+        POST /pools/default/stats/range
+        docs.couchbase.com/server/current/rest-api/rest-statistics-multiple.html
+        """
+        api = self.base_url + "/pools/default/stats/range"
+        status, content, _ = self.request(api, CBRestConnection.POST,
+                                          params=data)
+        return status, content
