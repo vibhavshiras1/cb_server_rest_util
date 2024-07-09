ba129c73cdbaf81206ab66ede9bf3a259a8120c7
Ashwin G <ashwintrojan+github@gmail.com>
2024-07-09 11:06:02 +0530

    Adding bucket::get_random_key() API

--- buckets/buckets_api.py
+++ buckets/buckets_api.py
@@ -1,11 +1,13 @@
 from cb_server_rest_util.buckets.bucket_info import BucketInfo
 from cb_server_rest_util.buckets.bucket_stats import BucketStats
+from cb_server_rest_util.buckets.doc_ops import DocOpAPI
 from cb_server_rest_util.buckets.manage_bucket import BucketManageAPI
 from cb_server_rest_util.buckets.scope_and_collections import \
     ScopeAndCollectionsAPI
 
 
 class BucketRestApi(BucketManageAPI, BucketInfo, BucketStats,
+                    DocOpAPI,
                     ScopeAndCollectionsAPI):
     def __init__(self, server):
         super(BucketRestApi, self).__init__()
