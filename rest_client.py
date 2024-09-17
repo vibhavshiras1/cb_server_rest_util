a108b6fb604153f5ac8b930306a4bd8126e97417
Umang Agrawal <umang.agrawal@couchbase.com>
2024-09-17 12:43:43 +0530

    Adding all APIs in activate_service_api

--- rest_client.py
+++ rest_client.py
@@ -1,8 +1,13 @@
+from cb_constants.CBServer import CbServer
 from cb_server_rest_util.analytics.analytics_api import AnalyticsRestAPI
+from cb_server_rest_util.backup.backup_api import BackupRestApi
+from cb_server_rest_util.buckets.buckets_api import BucketRestApi
 from cb_server_rest_util.cluster_nodes.cluster_nodes_api import ClusterRestAPI
 from cb_server_rest_util.index.index_api import IndexRestAPI
 from cb_server_rest_util.query.query_api import QueryRestAPI
-from cb_constants.CBServer import CbServer
+from cb_server_rest_util.security.security_api import SecurityRestAPI
+from cb_server_rest_util.server_groups.server_groups_api import ServerGroupsAPI
+from cb_server_rest_util.xdcr.xdcr_api import XdcrRestAPI
 
 
 class RestConnection(object):
@@ -10,12 +15,18 @@ class RestConnection(object):
         self.server = server
 
         self.cluster = ClusterRestAPI(self.server)
+        self.bucket = BucketRestApi(self.server)
+
         self.index = None
         self.query = None
         self.analytics = None
         self.eventing = None
         self.fts = None
         self.backup = None
+
+        self.security = None
+        self.server_group = None
+        self.xdcr = None
 
     def activate_service_api(self, service_list):
         if CbServer.Services.INDEX in service_list:
@@ -29,4 +40,10 @@ class RestConnection(object):
         if CbServer.Services.FTS in service_list:
             pass
         if CbServer.Services.BACKUP in service_list:
-            pass
+            self.backup = BackupRestApi(self.server)
+        if "security" in service_list:
+            self.security = SecurityRestAPI(self.server)
+        if "server_group" in service_list:
+            self.server_group = ServerGroupsAPI(self.server)
+        if "xdcr" in service_list:
+            self.xdcr = XdcrRestAPI(self.server)
