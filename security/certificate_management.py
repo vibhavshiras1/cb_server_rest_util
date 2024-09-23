cc69c9e5fc1ee95a77e340c653c4687268752cc8
Umang Agrawal <63640118+umang-cb@users.noreply.github.com>
2024-09-23 15:33:09 +0530

    Update and rename certificates.py to certificate_management.py

--- security/certificate_management.py
+++ security/certificate_management.py
@@ -1,14 +1,14 @@
 from cb_server_rest_util.connection import CBRestConnection
 
 
-class Certicates(CBRestConnection):
+class CertificateMangementAPI(CBRestConnection):
     def __init__(self):
         super(Certicates, self).__init__()
 
     def get_trusted_root_certificates(self):
         """
         GET:: /pools/default/trustedCAs
-        docs.couchbase.com/server/current/manage/manage-xdcr/secure-xdcr-replication.html#capella-trusted-cas
+        https://docs.couchbase.com/server/current/rest-api/deprecated-security-apis/upload-retrieve-root-cert.html#examples
         """
         url = "/pools/default/trustedCAs"
         api = self.base_url + url
@@ -16,6 +16,10 @@ class Certicates(CBRestConnection):
         return status, content
 
     def get_node_certificates(self):
+        """
+        GET:: /pools/default/certificates
+        https://docs.couchbase.com/server/current/rest-api/deprecated-security-apis/upload-retrieve-root-cert.html#examples
+        """
         url = "/pools/default/certificates"
         api = self.base_url + url
         status, content, _ = self.request(api, self.GET)
