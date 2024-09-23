405f65cd10cd854f8ae62b2e58046d56b4ae9f9d
Umang Agrawal <63640118+umang-cb@users.noreply.github.com>
2024-09-23 15:34:26 +0530

    Update security_api.py

--- security/security_api.py
+++ security/security_api.py
@@ -1,11 +1,11 @@
 from cb_server_rest_util.security.auditing import Auditing
-from cb_server_rest_util.security.certificates import Certicates
+from cb_server_rest_util.security.certificate_management import CertificateMangementAPI
 from cb_server_rest_util.security.rbac_authorization import RbacAuthorization
 from cb_server_rest_util.security.restrict_node_addition import \
     NodeInitAddition
 
 
-class SecurityRestAPI(Auditing, Certicates, NodeInitAddition,
+class SecurityRestAPI(Auditing, CertificateMangementAPI, NodeInitAddition,
                       RbacAuthorization):
     def __init__(self, server):
         super(SecurityRestAPI, self).__init__()
