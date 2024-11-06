57f3b2c52faaf475333ec74d640ec4e5cbfb5ed0
Ashwin G <ashwintrojan+github@gmail.com>
2024-11-06 11:10:33 +0530

    Adding APIs for system_event_logs

--- cluster_nodes/status_and_events.py
+++ cluster_nodes/status_and_events.py
@@ -89,3 +89,38 @@ class StatusAndEventsAPI(CBRestConnection):
         api = self.base_url + '/logClientError'
         status, json_parsed, _ = self.request(api, params=msg)
         return status, json_parsed
+
+    def get_system_event_logs(self, since_time=None, limit=None):
+        """
+        GET :: /events?sinceTime=<since_time>&limit=<limit>
+        docs.couchbase.com/server/current/rest-api/rest-get-system-events.html
+        """
+        api = self.base_url + '/events'
+        params = dict()
+        if since_time:
+            params["sinceTime"] = since_time
+        if limit:
+            params["limit"] = limit
+        status, json_parsed, _ = self.request(api, params=params)
+        return status, json_parsed
+
+    def get_system_event_streaming(self):
+        """
+        GET :: /eventsStreaming
+        docs.couchbase.com/server/current/rest-api/rest-get-system-events.html
+        """
+        api = self.base_url + '/eventsStreaming'
+        status, json_parsed, _ = self.request(api)
+        return status, json_parsed
+
+    def create_system_event(self, event_dict):
+        """
+        POST :: /_event
+        Not documented in CB docs
+        """
+        api = self.base_url + "/_event"
+        header = self.get_headers_for_content_type_json()
+        status, content, _ = self.request(
+            api, method=self.POST, headers=header,
+            params=self.flatten_param_to_str(event_dict))
+        return status, content
