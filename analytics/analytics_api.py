c23281967ac2e4de827694e67e45598105490746
Vibhav S P <vibhavshiraspramod@gmail.com>
2025-01-07 17:55:36 +0530

    Adding Analytics API Functions

--- analytics/analytics_api.py
+++ analytics/analytics_api.py
@@ -1,7 +1,8 @@
 from cb_server_rest_util.analytics.analytics_settings import AnalyticsSettingsAPI
+from cb_server_rest_util.analytics.analytics_functions import AnalyticsFunctionsAPI
 
 
-class AnalyticsRestAPI(AnalyticsSettingsAPI):
+class AnalyticsRestAPI(AnalyticsSettingsAPI, AnalyticsFunctionsAPI):
     def __init__(self, server):
         super(AnalyticsRestAPI).__init__()
 
