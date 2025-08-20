32ddb4a1c9d1b226303eeba730739b136c0636bd
Vibhav S P <vibhavshiraspramod@gmail.com>
2025-08-20 16:53:24 +0530

    Adding Fusion APIs - Enable/Disable/Stop Fusion - Get Fusion Status - Manage Fusion Settings - logStoreURI, sync_threshold

--- fusion/fusion_functions.py
+++ fusion/fusion_functions.py
@@ -11,3 +11,61 @@ class FusionFunctions(CBRestConnection):
         api = self.base_url + "/fusion/activeGuestVolumes"
         status, content, _ = self.request(api)
         return status, content
+    
+    def manage_fusion_settings(self, log_store_uri=None, enable_sync_threshold=None):
+        """
+        POST / GET :: /settings/fusion
+        """
+        api = self.base_url + "/settings/fusion"
+
+        params = dict()
+        if log_store_uri is not None:
+            params["logStoreURI"] = log_store_uri
+        if enable_sync_threshold is not None:
+            params["enableSyncThresholdMB"] = enable_sync_threshold
+        if params:
+            # POST method
+            status, _, response = self.request(api, CBRestConnection.POST,
+                                               params=params)
+        else:
+            # GET method
+            status, _, response = self.request(api, CBRestConnection.GET)
+        content = response.json() if status else response.text
+        return status, content
+
+    def get_fusion_status(self):
+        """
+        GET :: /fusion/status
+        """
+        api = self.base_url + "/fusion/status"
+        status, content, _ = self.request(api)
+        return status, content
+
+    def enable_fusion(self, buckets=None):
+        """
+        POST :: /fusion/enable
+        """
+        api = self.base_url + "/fusion/enable"
+        if buckets is not None:
+            params = dict()
+            params["buckets"] = buckets
+            status, content, _ = self.request(api, CBRestConnection.POST, params=params)
+        else:
+            status, content, _ = self.request(api, CBRestConnection.POST)
+        return status, content
+
+    def disable_fusion(self):
+        """
+        POST :: /fusion/disable
+        """
+        api = self.base_url + "/fusion/disable"
+        status, content, _ = self.request(api, CBRestConnection.POST)
+        return status, content
+
+    def stop_fusion(self):
+        """
+        POST :: /fusion/stop
+        """
+        api = self.base_url + "/fusion/stop"
+        status, content, _ = self.request(api, CBRestConnection.POST)
+        return status, content
